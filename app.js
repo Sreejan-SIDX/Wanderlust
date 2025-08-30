7bad138373a019e0034cc859c81f43f5c36bc861
Sreejan-SIDX <mr.sreejandhar@gmail.com>
2025-08-31 03:42:29 +0530

    Add home route to fix Cannot GET /

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ const sessionOptions = {
 };
 
 
-// app.get("/", (req, res) => {
-//     res.send("Hi, I am root");
-// });
+app.get('/', (req, res) => {
+  res.render('home'); // or res.send('Home Page')
+});
 
 
 app.use(session(sessionOptions));
